220ae774c7203296a3adc75e831fdbdb1e202d8d
Issam SAID <said.issam@gmail.com>
2016-07-26 13:35:23 +0200

    add OpenCL support to the travis-ci config file

--- cmake/FindOpenCL.cmake
+++ cmake/FindOpenCL.cmake
@@ -37,114 +37,63 @@
 ## 		- AMD APP SDK 
 ##    - NVIDIA SDK
 ##    - Apple SDK
+##    - Intel SDK
 ## 
 ## It defines the following variables:
 ##      - OPENCL_FOUND (found the OPENCL framework)
-##      - OPENCL_INCLUDE_DIRS (include directories)
+##      - OPENCL_INCLUDE_DIRS (OpenCL headers directories)
+##      - OPENCL_LIBRARIES (OpenCL libraries names)
 ##
-## It also defines the library variables below:
-##      - OPENCL_LIBRARIES libOpenCL
-##
-## It accepts the following variables as input:
-## @param OPENCL_INCLUDE_DIR the root directory of the OpenCL implementation 
-## headers can be found.
+## It accepts the following variables as inputs, which will overwrite the 
+## behavior of the package locater:
+## @param OPENCL_INCLUDE_DIRS if is set by the user it will be used instead of 
+## script hints, to locate the directories of the OpenCL headers.
+## @param OPENCL_LIBRARY_DIRS if is set by the user it will be used to locate 
+## the OpenCL library. Note that the OpenCL library path can be manually added 
+## to the variable LD_LIBRARY_PATH.
 ##     
-function(_FIND_OPENCL_VERSION)
-  include(CheckSymbolExists)
-  include(CMakePushCheckState)
-  set(CMAKE_REQUIRED_QUIET ${OpenCL_FIND_QUIETLY})
-
-  CMAKE_PUSH_CHECK_STATE()
-  foreach(VERSION "2_0" "1_2" "1_1" "1_0")
-    set(CMAKE_REQUIRED_INCLUDES "${OpenCL_INCLUDE_DIR}")
-
-    if(APPLE)
-      CHECK_SYMBOL_EXISTS(
-        CL_VERSION_${VERSION}
-        "${OpenCL_INCLUDE_DIR}/OpenCL/cl.h"
-        OPENCL_VERSION_${VERSION})
-    else()
-      CHECK_SYMBOL_EXISTS(
-        CL_VERSION_${VERSION}
-        "${OpenCL_INCLUDE_DIR}/CL/cl.h"
-        OPENCL_VERSION_${VERSION})
-    endif()
-
-    if(OPENCL_VERSION_${VERSION})
-      string(REPLACE "_" "." VERSION "${VERSION}")
-      set(OpenCL_VERSION_STRING ${VERSION} PARENT_SCOPE)
-      string(REGEX MATCHALL "[0-9]+" version_components "${VERSION}")
-      list(GET version_components 0 major_version)
-      list(GET version_components 1 minor_version)
-      set(OpenCL_VERSION_MAJOR ${major_version} PARENT_SCOPE)
-      set(OpenCL_VERSION_MINOR ${minor_version} PARENT_SCOPE)
-      break()
-    endif()
-  endforeach()
-  CMAKE_POP_CHECK_STATE()
-endfunction()
-
-find_path(OPENCL_INCLUDE_DIRS
+if (not OPENCL_INCLUDE_DIRS)
+  find_path(OPENCL_INCLUDE_DIRS
     NAMES OpenCL/cl.h CL/cl.h
     HINTS
-        ${OPENCL_INCLUDE_DIR}/
+        $ENV{AMDAPPSDK}/include
         $ENV{AMDAPPSDKROOT}/include
+        $ENV{ATISTREAMSDKROOT}/include
         $ENV{CUDA_PATH}/include
-        $ENV{AMDAPPSDKROOT}/include
+        $ENV{CUDA_ROOT}/include
         $ENV{INTELOCLSDKROOT}/include
         $ENV{NVSDKCOMPUTE_ROOT}/include
-        $ENV{ATISTREAMSDKROOT}/include
     PATHS
         /usr/include
         /usr/local/include
         /usr/local/cuda/include
         /opt/cuda/include
-    DOC "OpenCL header file path"
-)
-mark_as_advanced( OPENCL_INCLUDE_DIRS )
-
-if (OPENCL_FOUND)
-_FIND_OPENCL_VERSION()
+    DOC "OpenCL headers possible paths"
+  )
+  mark_as_advanced(OPENCL_INCLUDE_DIRS)
 endif()
 
-## Search for 64bit libs if FIND_LIBRARY_USE_LIB64_PATHS is set to 
-## true in the global environment, 32bit libs else
-get_property( LIB64 GLOBAL PROPERTY FIND_LIBRARY_USE_LIB64_PATHS )
-
-if( LIB64 )
-    find_library( OPENCL_LIBRARIES
-        NAMES OpenCL
-        HINTS
-            ${OPENCL_ROOT}/lib
-            $ENV{AMDAPPSDKROOT}/lib
-            $ENV{CUDA_PATH}/lib
-        DOC "OpenCL dynamic library path"
-        PATH_SUFFIXES x86_64 x64 x86_64/sdk
-        PATHS
-            /usr/lib
-            /usr/local/cuda/lib
-            /opt/cuda/lib
-    )
-else( )
-    find_library( OPENCL_LIBRARIES
-        NAMES OpenCL
-        HINTS
-            ${OPENCL_ROOT}/lib
-            $ENV{AMDAPPSDKROOT}/lib
-            $ENV{CUDA_PATH}/lib
-        DOC "OpenCL dynamic library path"
-        PATH_SUFFIXES x86 Win32
-        
-        PATHS
-            /usr/lib
-            /usr/local/cuda/lib
-            /opt/cuda/lib
-    )
+find_library(OPENCL_LIBRARIES
+  NAMES OpenCL
+  HINTS
+      $ENV{OPENCL_LIBRARY_DIRS}
+      $ENV{AMDAPPSDKROOT}/lib
+      $ENV{AMDAPPSDK}/lib
+      $ENV{ATISTREAMSDKROOT}/lib
+      $ENV{CUDA_PATH}/lib
+      $ENV{CUDA_ROOT/lib
+  DOC "OpenCL dynamic libraries paths"
+  PATH_SUFFIXES x86 Win32 x86_64 x64 x86_64/sdk
+  PATHS
+    /usr/lib
+    /usr/local/cuda/lib
+    /opt/cuda/lib
+  )
 endif( )
-mark_as_advanced( OPENCL_LIBRARIES )
+mark_as_advanced(OPENCL_LIBRARIES)
 
-include( FindPackageHandleStandardArgs )
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(OPENCL DEFAULT_MSG 
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(OPENCL 
+                                  DEFAULT_MSG 
                                   OPENCL_LIBRARIES 
-                                  OPENCL_INCLUDE_DIRS )
-
+                                  OPENCL_INCLUDE_DIRS)
