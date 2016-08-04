80c9097eb701c4f88099613d3bfcd193069a7ac3
Issam SAID <said.issam@gmail.com>
2016-08-04 08:28:40 +0200

    fix intel compiler warnings

--- cmake/intel_c.cmake
+++ cmake/intel_c.cmake
@@ -3,7 +3,7 @@ set(CMAKE_C_FLAGS_DEBUG
 set(CMAKE_C_FLAGS_DEBUG   
     "${CMAKE_C_FLAGS_DEBUG} -fp-stack-check -fp-model strict")
 set(CMAKE_C_FLAGS_RELEASE 
-    "${CMAKE_C_FLAGS_RELEASE} -O3 -xSSSE3  -fno-fnalias -fno-alias -vec-report1")
+    "${CMAKE_C_FLAGS_RELEASE} -O3 -fno-fnalias -fno-alias -vec-report1")
 find_package(OpenMP)
 if (OPENMP_FOUND)
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
--- cmake/intel_cxx.cmake
+++ cmake/intel_cxx.cmake
@@ -3,7 +3,7 @@ set(CMAKE_CXX_FLAGS_DEBUG
 set(CMAKE_CXX_FLAGS_DEBUG   
     "${CMAKE_CXX_FLAGS_DEBUG} -fp-stack-check -fp-model strict")
 set(CMAKE_CXX_FLAGS_RELEASE 
-    "${CMAKE_CXX_FLAGS_RELEASE} -O3 -xSSSE3 -fno-fnalias -fno-alias -vec-report1")
+    "${CMAKE_CXX_FLAGS_RELEASE} -O3 -fno-fnalias -fno-alias -vec-report1")
 find_package(OpenMP)
 if (OPENMP_FOUND)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_C_FLAGS}")
