5919002eb713f98b826be73e443cbeb7dd084cb6
Issam SAID <said.issam@gmail.com>
2016-10-04 13:35:29 +0200

    add the devices_info C/C++ example

---
+++ examples/src/devices_info/main.c
@@ -0,0 +1,60 @@
+///
+/// @copyright Copyright (c) 2013-2016, Univrsité Pierre et Marie Curie
+/// All rights reserved.
+///
+/// <b>hiCL</b> is owned by Université Pierre et Marie Curie (UPMC),
+/// funded by TOTAL, and written by Issam SAID <said.issam@gmail.com>.
+///
+/// Redistribution and use in source and binary forms, with or without
+/// modification, are permitted provided that the following conditions
+/// are met:
+///
+/// 1. Redistributions of source code must retain the above copyright
+///    notice, this list of conditions and the following disclaimer.
+/// 2. Redistributions in binary form must reproduce the above copyright
+///    notice, this list of conditions and the following disclaimer in the
+///    documentation and/or other materials provided with the distribution.
+/// 3. Neither the name of the UPMC nor the names of its contributors
+///    may be used to endorse or promote products derived from this software
+///    without specific prior written permission.
+///
+/// THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+/// INCLUDING, BUT NOT LIMITED TO, WARRANTIES OF MERCHANTABILITY AND FITNESS
+/// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE UPMC OR
+/// ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+/// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+/// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+/// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+/// LIABILITY, WETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+/// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+/// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+///
+/// @file devices_info/main.c
+/// @author Issam SAID
+/// @brief An example of OpenCL devices quey based on the hiCL C/C++ interface.
+///
+#include <stdio.h>
+#include <stdlib.h>
+#include <hiCL/hiCL.h>
+
+///
+/// @brief The main program of the hiCL devices query example.
+///
+/// This is the main routine that shows how to use the hiCL C/C++ interface 
+/// to implement a simple OpenCL devices query program.
+/// @return Error code if any.
+///
+int main(void) {
+    fprintf(stdout, "... start of the hiCL devices info example\n");
+
+    ///< Initialize hiCL with all possible devices.
+    hicl_init(ALL);
+
+    ///< Show the information of the loaded devices.
+    hicl_info();
+
+    ///< Release hiCL resources.
+    hicl_release();
+    fprintf(stdout, "... end   of the hiCL devices info example\n");
+    return EXIT_SUCCESS;
+}
